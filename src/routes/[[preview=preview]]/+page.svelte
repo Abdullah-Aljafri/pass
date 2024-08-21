a8953c2db1816311b023a45376956d6722f244a9
Abdullah Aljufry <Abdullah.mashi2015@gmail.com>
2024-08-21 10:19:46 +0300

    initial commit

---
+++ src/routes/[[preview=preview]]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { SliceZone } from '@prismicio/svelte';
+
+	import { components } from '$lib/slices/index.js';
+
+	export let data;
+</script>
+
+<SliceZone slices={data.slices} {components} />
