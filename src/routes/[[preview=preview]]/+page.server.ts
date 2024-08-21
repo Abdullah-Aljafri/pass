0d4f1a6facd5e3829c96a52cb9ff04c599c2506c
Abdullah Aljufry <Abdullah.mashi2015@gmail.com>
2024-08-21 12:52:01 +0300

    mapper file1

--- src/routes/[[preview=preview]]/+page.server.ts
+++ src/routes/[[preview=preview]]/+page.server.ts
@@ -1,15 +1,15 @@
 import { asText, mapSliceZone } from '@prismicio/client';
 
 import { createClient } from '$lib/prismicio';
-// import { mappers } from '$lib/slices/mappers';
+import { mappers } from '$lib/slices/mappers';
 
 export async function load({ fetch, cookies }) {
 	const client = createClient({ fetch, cookies });
 
 	const page = await client.getByUID('page', 'home');
-    // const slices = await mapSliceZone(page.data.slices, mappers, { client });
+    const slices = await mapSliceZone(page.data.slices, mappers, { client });
 	return {
-		// slices,
+		slices,
 		title: asText(page.data.title),
 		meta_description: page.data.meta_description,
 		meta_title: page.data.meta_title,
